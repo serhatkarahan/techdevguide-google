282aa7546193dd13456a938250f28403daf7dc2c
Serhat Karahan <serhat.karahan@iyzico.com>
2019-11-25 12:42:54 +0300

    first commit

---
+++ src/main/java/foundations/FindLongestWord.java
@@ -0,0 +1,8 @@
+package foundations;
+
+public class FindLongestWord {
+
+    public static String solution() {
+        return null;
+    }
+}
---
+++ src/test/java/foundations/FindLongestWordTest.java
@@ -0,0 +1,15 @@
+package foundations;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FindLongestWordTest {
+
+    @Test
+    public void solution() {
+        List.of("a");
+    }
+}
