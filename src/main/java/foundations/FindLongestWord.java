c5a8e85ad03e19cec4a4cb6f84c3ebdbb8c18435
Serhat Karahan <serhat.karahan@iyzico.com>
2019-11-26 20:02:38 +0300

    adds test case for multiple words

--- src/main/java/foundations/FindLongestWord.java
+++ src/main/java/foundations/FindLongestWord.java
@@ -1,8 +1,13 @@
 package foundations;
 
+import java.util.List;
+
 public class FindLongestWord {
 
-    public static String solution() {
-        return null;
+    public static String solution(String s, List<String> d) {
+        return d.stream()
+                .filter(word -> word.equals(s))
+                .findAny()
+                .orElse("");
     }
 }
--- src/test/java/foundations/FindLongestWordTest.java
+++ src/test/java/foundations/FindLongestWordTest.java
@@ -4,12 +4,47 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class FindLongestWordTest {
 
     @Test
-    public void solution() {
-        List.of("a");
+    public void returns_empty_string_when_s_is_empty() {
+        //given
+        String s = "";
+        List<String> d = List.of("a");
+
+        //when
+        String result = FindLongestWord.solution(s, d);
+
+        //then
+        assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void returns_the_only_word_when_s_has_the_string() {
+        //given
+        String s = "a";
+        List<String> d = List.of("a");
+
+        //when
+        String result = FindLongestWord.solution(s, d);
+
+        //then
+        assertThat(result).isEqualTo("a");
+    }
+
+    @Test
+    public void returns_the_matching_word_when_d_has_multiple_words() {
+        //given
+        String s = "a";
+        List<String> d = List.of("b", "a");
+
+        //when
+        String result = FindLongestWord.solution(s, d);
+
+        //then
+        assertThat(result).isEqualTo("a");
     }
 }
