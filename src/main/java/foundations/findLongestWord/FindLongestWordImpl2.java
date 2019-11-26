ac203bb31d02abdc29db5d3475d0478a80874978
Serhat Karahan <serhat.karahan@iyzico.com>
2019-11-26 22:30:16 +0300

    finishes first draft of char position map solution

--- src/main/java/foundations/findLongestWord/FindLongestWordImpl2.java
+++ src/main/java/foundations/findLongestWord/FindLongestWordImpl2.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/*
+    Preprocesses given string by building a char position map, therefore finding if a word is a subsequence of string is much faster
+ */
 public class FindLongestWordImpl2 implements FindLongestWord {
 
     public String solution(String s, List<String> d) {
+        String result = "";
         Map<Character, List<Integer>> sCharPositions = buildCharPositions(s);
-        return "";
-
+        for (String word : d) {
+            if (isSubsequence(sCharPositions, word) && isLongerWord(result, word)) {
+                result = word;
+            }
+        }
+        return result;
     }
 
     private Map<Character, List<Integer>> buildCharPositions(String s) {
@@ -22,5 +29,31 @@ public class FindLongestWordImpl2 implements FindLongestWord {
             sCharPositions.put(c, positions);
         }
         return sCharPositions;
+    }
+
+    private boolean isLongerWord(String oldWord, String newWord) {
+        return newWord.length() > oldWord.length();
+    }
+
+    private boolean isSubsequence(Map<Character, List<Integer>> sCharPositions, String word) {
+        int currentPositionOnGivenString = -1;
+        for (char c : word.toCharArray()) {
+            List<Integer> letterPositions = sCharPositions.get(c);
+            if (letterPositions == null) {
+                return false;
+            }
+            boolean notFound = true;
+            for (int position : letterPositions) {
+                if (position > currentPositionOnGivenString) {
+                    currentPositionOnGivenString = position;
+                    notFound = false;
+                    break;
+                }
+            }
+            if (notFound) {
+                return false;
+            }
+        }
+        return true;
     }
 }
