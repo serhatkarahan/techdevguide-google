f42204831c1b28f8b73bae1c90fa96faba6fe195
Serhat Karahan <serhat.karahan@iyzico.com>
2019-11-27 23:58:06 +0300

    adds solution for repeating single letter

--- src/main/java/foundations/findLongestWord/CharPositionDenseBuilder.java
+++ src/main/java/foundations/findLongestWord/CharPositionDenseBuilder.java
@@ -5,13 +5,21 @@ import java.util.*;
 public class CharPositionDenseBuilder {
 
     public Map<Character, List<Integer>> buildDense(String s) {
-        Map<Character, List<Integer>> result = new HashMap<>();
-        result.put('a', new ArrayList<>());
-        for (int i = 0; i < s.length(); i++) {
+        Map<Character, List<Integer>> result = initializeResult();
+        for (int i = 1; i < s.length(); i++) {
             char currentChar = s.charAt(i);
             List<Integer> positionList = result.get(currentChar);
             positionList.add(i);
             result.put(currentChar, positionList);
+        }
+        result.values().forEach(positionList -> positionList.add(-1));
+        return result;
+    }
+
+    private Map<Character, List<Integer>> initializeResult() {
+        Map<Character, List<Integer>> result = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            result.put((char)('a' + i), new ArrayList<>());
         }
         return result;
     }
