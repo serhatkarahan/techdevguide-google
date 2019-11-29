eb8cdc02b40685954fbfa177c5fd71b58ff49b49
Serhat Karahan <serhat.karahan@iyzico.com>
2019-11-30 01:12:54 +0300

    refactor

--- src/main/java/foundations/stringSplosion/StringSplosion.java
+++ src/main/java/foundations/stringSplosion/StringSplosion.java
@@ -3,10 +3,10 @@ package foundations.stringSplosion;
 public class StringSplosion {
 
     public String solution(String s) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 1; i <= s.length(); i++) {
-            result = result + s.substring(0, i);
+            result.append(s, 0, i);
         }
-        return result;
+        return result.toString();
     }
 }
