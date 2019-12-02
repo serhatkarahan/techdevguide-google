e9f91243bbec7e5d7ac0272e73ad963a578d3ad8
Serhat Karahan <serhat.karahan@iyzico.com>
2019-12-02 21:30:11 +0300

    adds solution

--- src/main/java/foundations/canBalance/CanBalance.java
+++ src/main/java/foundations/canBalance/CanBalance.java
@@ -3,6 +3,35 @@ package foundations.canBalance;
 public class CanBalance {
 
     public boolean canBalance(int[] nums) {
-        return true;
+        if (nums.length < 2) {
+            return false;
+        }
+        int left = -1;
+        int right = nums.length;
+        int sumLeft = 0;
+        int sumRight = 0;
+        while (left < right) {
+            if (sumLeft == sumRight) {
+                if (right - left > 1) {
+                    left++;
+                    sumLeft += nums[left];
+                    right--;
+                    sumRight += nums[right];
+                } else if (right - left == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (sumLeft < sumRight) {
+                    left++;
+                    sumLeft += nums[left];
+                } else {
+                    right--;
+                    sumRight += nums[right];
+                }
+            }
+        }
+        return false;
     }
 }
