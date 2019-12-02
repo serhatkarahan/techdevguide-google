086dcc4c0223a1bc81427389f2311fcb349e93fe
Serhat Karahan <serhat.karahan@iyzico.com>
2019-12-02 19:35:15 +0300

    adds solution

--- src/main/java/foundations/withoutString/WithoutString.java
+++ src/main/java/foundations/withoutString/WithoutString.java
@@ -3,6 +3,6 @@ package foundations.withoutString;
 public class WithoutString {
 
     public String solution(String base, String remove) {
-        return base.replace(remove, "");
+        return base.toLowerCase().replace(remove.toLowerCase(), "");
     }
 }
