f2e873ddea16d625ee793191026001ee55eb6003
Serhat Karahan <serhat.karahan@iyzico.com>
2019-12-06 11:45:48 +0300

    initializes number of guesses and word to be guessed

--- src/main/java/foundations/hangman/Game.java
+++ src/main/java/foundations/hangman/Game.java
@@ -1,12 +1,31 @@
 package foundations.hangman;
 
+import java.util.List;
+import java.util.Random;
+
 public class Game {
+
+    private static final int NUMBER_OF_ALLOWED_GUESSES = 8;
+    private static final List<String> WORDS = List.of("BUOY", "COMPUTER", "CONNOISSEUR", "DEHYDRATE", "FUZZY", "HUBBUB", "KEYHOLE", "QUAGMIRE", "SLITHER", "ZIRCON");
+
+    private String word;
+    private int numberOfGuessesLeft;
 
     public void play(){
         welcome();
+        initialize();
     }
 
     private void welcome() {
         System.out.println("Welcome to Hangman!");
+    }
+
+    private void initialize() {
+        numberOfGuessesLeft = NUMBER_OF_ALLOWED_GUESSES;
+        word = retrieveRandomWord();
+    }
+
+    private String retrieveRandomWord() {
+        return WORDS.get(new Random().nextInt(WORDS.size()));
     }
 }
