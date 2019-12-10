875c684dc99fea7232c456c2b8b0c412db812032
Serhat Karahan <serhat.karahan@iyzico.com>
2019-12-10 18:14:50 +0300

    adds test case and solution

--- src/main/java/foundations/mapShare/MapShare.java
+++ src/main/java/foundations/mapShare/MapShare.java
@@ -5,6 +5,9 @@ import java.util.Map;
 public class MapShare {
 
     public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.get("a") != null) {
+            map.put("b", map.get("a"));
+        }
         return map;
     }
 }
--- src/test/java/foundations/mapShare/MapShareTest.java
+++ src/test/java/foundations/mapShare/MapShareTest.java
@@ -31,4 +31,20 @@ public class MapShareTest {
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get("d")).isEqualTo("d");
     }
+
+    @Test
+    public void should_set_value_of_a_to_b_when_map_has_key_a() {
+        //given
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "value");
+
+        //when
+        Map<String, String> result = mapShare.mapShare(map);
+
+        ///then
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get("a")).isEqualTo("value");
+        assertThat(result.get("b")).isEqualTo("value");
+    }
 }
