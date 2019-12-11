f32e68b96350afec4cc1835c3a0eb42e9692c1c8
Serhat Karahan <serhat.karahan@iyzico.com>
2019-12-11 14:51:41 +0300

    adds solution

--- src/main/java/foundations/sortArray/SortImpl1.java
+++ src/main/java/foundations/sortArray/SortImpl1.java
@@ -1,9 +1,14 @@
 package foundations.sortArray;
 
+import java.util.Arrays;
+
 public class SortImpl1 implements Sort {
 
     @Override
     public int[] sort(int[] input) {
+        if (input.length > 0) {
+            return Arrays.copyOfRange(input, 0, 1);
+        }
         return input;
     }
 }
