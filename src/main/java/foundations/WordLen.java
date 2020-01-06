94a343b96760eb98bd486b9c9eb425401d1ce418
Serhat Karahan <serhat.karahan@iyzico.com>
2020-01-06 19:20:29 +0300

    adds solution and tests

--- src/main/java/foundations/WordLen.java
+++ src/main/java/foundations/WordLen.java
@@ -1,10 +1,15 @@
 package foundations;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordLen {
 
     public Map<String, Integer> wordLen(String[] strings) {
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        for (String s : strings) {
+            result.put(s, s.length());
+        }
+        return result;
     }
 }
--- src/test/java/foundations/WordLenTest.java
+++ src/test/java/foundations/WordLenTest.java
@@ -3,20 +3,59 @@ package foundations;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordLenTest {
 
+    private WordLen wordLen;
+
     @Before
     public void setUp() {
+        wordLen = new WordLen();
     }
 
     @Test
     public void case_1() {
         //given
+        String[] strings = {"a", "bb", "a", "bb"};
 
         //when
+        Map<String, Integer> result = wordLen.wordLen(strings);
 
         ///then
+        assertThat(result).hasSize(2);
+        assertThat(result.get("a")).isEqualTo(1);
+        assertThat(result.get("bb")).isEqualTo(2);
+    }
+
+    @Test
+    public void case_2() {
+        //given
+        String[] strings = {"this", "and", "that", "and"};
+
+        //when
+        Map<String, Integer> result = wordLen.wordLen(strings);
+
+        ///then
+        assertThat(result).hasSize(3);
+        assertThat(result.get("that")).isEqualTo(4);
+        assertThat(result.get("and")).isEqualTo(3);
+        assertThat(result.get("this")).isEqualTo(4);
+    }
+
+    @Test
+    public void case_3() {
+        //given
+        String[] strings = {"code", "code", "code", "bug"};
+
+        //when
+        Map<String, Integer> result = wordLen.wordLen(strings);
+
+        ///then
+        assertThat(result).hasSize(2);
+        assertThat(result.get("code")).isEqualTo(4);
+        assertThat(result.get("bug")).isEqualTo(3);
     }
 }
