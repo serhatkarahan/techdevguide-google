d4c4a88a17294eb29e077f83213509cfe1a01dc7
Serhat Karahan <serhat.karahan@iyzico.com>
2020-01-06 19:12:45 +0300

    adds solution and tests

--- src/main/java/foundations/wordZero/WordZero.java
+++ src/main/java/foundations/wordZero/WordZero.java
@@ -1,10 +1,15 @@
 package foundations.wordZero;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordZero {
 
     public Map<String, Integer> word0(String[] strings) {
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        for (String s : strings) {
+            result.put(s, 0);
+        }
+        return result;
     }
 }
--- src/test/java/foundations/wordZero/WordZeroTest.java
+++ src/test/java/foundations/wordZero/WordZeroTest.java
@@ -1,15 +1,62 @@
 package foundations.wordZero;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordZeroTest {
 
+    private WordZero wordZero;
+
+    @Before
+    public void setUp() {
+        wordZero = new WordZero();
+    }
+
     @Test
-    public void case_0() {
+    public void case_1() {
         //given
+        String[] strings = {"a", "b", "a", "b"};
 
         //when
+        Map<String, Integer> result = wordZero.word0(strings);
 
         ///then
+        assertThat(result).hasSize(2);
+        assertThat(result.get("a")).isEqualTo(0);
+        assertThat(result.get("b")).isEqualTo(0);
+    }
+
+    @Test
+    public void case_2() {
+        //given
+        String[] strings = {"a", "b", "a", "c", "b"};
+
+        //when
+        Map<String, Integer> result = wordZero.word0(strings);
+
+        ///then
+        assertThat(result).hasSize(3);
+        assertThat(result.get("a")).isEqualTo(0);
+        assertThat(result.get("b")).isEqualTo(0);
+        assertThat(result.get("c")).isEqualTo(0);
+    }
+
+    @Test
+    public void case_3() {
+        //given
+        String[] strings = {"c", "b", "a"};
+
+        //when
+        Map<String, Integer> result = wordZero.word0(strings);
+
+        ///then
+        assertThat(result).hasSize(3);
+        assertThat(result.get("a")).isEqualTo(0);
+        assertThat(result.get("b")).isEqualTo(0);
+        assertThat(result.get("c")).isEqualTo(0);
     }
 }
